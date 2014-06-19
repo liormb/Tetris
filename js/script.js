7b5bd2cec5e48c7c727eadb64deec4743ffb43a1
liormb <liormb@gmail.com>
2014-06-19 18:00:19 -0400

    adding board boundaries, clearing lines and shape stacking

--- js/script.js
+++ js/script.js
@@ -3,20 +3,22 @@
 //      Global Variables
 // --------------------------
 
+var $canvas;
+var $nextShape;
 var blockColor = 0;
 var blockSize = 32;
-var cols = 12;
-var rows = 16;
+var cols = 13;
+var rows = 20;
 var width  = cols * blockSize;
 var height = rows * blockSize;
-var ctx;
-var $canvas;
+var ctx, ntx;
 var board = [];
 var interval;
 var shape;
 var currentX, currentY; // position of current shape
-var speed = 1000;
+var speed = 800;
 var sprite;
+var gameOver = false;
 
 var shapes = [
   [ 1, 1, 1, 1 ],
@@ -44,6 +46,7 @@ function setCanvasSize(event, w, h){
 	$canvas.height = height || h || window.innerHeight || documentElement.clientHeight;
 }
 
+// draw the canvas grid
 function drawGrid(){
 	ctx = $canvas.getContext('2d');
 
@@ -62,6 +65,17 @@ function drawGrid(){
 	}
 }
 
+// draw the current board state
+function drawBoard(){
+	for (var row=0; row < rows; row++){
+		for (var col=0; col < cols; col++){
+			if (board[row][col])
+				drawBlock(col, row, board[row][col]);
+		}
+	}
+}
+
+// draw each single block piece
 function drawBlock(x, y, color){
 	ctx.drawImage(
 		sprite,
@@ -76,10 +90,8 @@ function drawBlock(x, y, color){
 	);
 }
 
-function renderDisplay(){
-	ctx.clearRect(0, 0, $canvas.width, $canvas.height);
-	drawGrid();
-
+// draw the current shape
+function drawShape(){
 	for (var y=0; y < 4; y++){
 		for (var x=0; x < 4; x++){
 			if (shape[y][x]) drawBlock(currentX + x, currentY + y, blockColor);
@@ -87,51 +99,16 @@ function renderDisplay(){
 	}
 }
 
-function rotate(shape){
-	var newShape = [];
-	var bool = true;
-	
-	// flipping the shape
-	for (var y=0; y < 4; y++){
-		newShape[y] = [];
-		for (var x=0, i=4; x < 4; x++, i--){
-			newShape[y][x] = shape[i-1][y] || 0;
-		}
-	}
+// --------------------------
+//          Game Logic
+// --------------------------
 
-	// aligning the shape to the left
-	while (bool){
-		for (var x=0; x < 4; x++){
-			if (newShape[x][0] !== 0) bool = false;
-		}
-		if (bool){
-			for (var i=0; i < 4; i++){
-				newShape[i].push(0);
-				newShape[i].shift();
-			}
-		}
-	}
-
-	return newShape;
-}
-
-
-function tick(){
-	if ( valid() ){
-		currentY++;
-	} else {
-		// stopShape();
-		newShape();
-	}
-}
-
-function valid(offsetX, offsetY, shape){
-	// TODO: set the game borders
-	return true;
-}
-
-function stopShape(){
-	// TODO: stop the shape when touching other shapes
+// render the canvas display
+function renderDisplay(){
+	ctx.clearRect(0, 0, $canvas.width, $canvas.height);
+	drawGrid();
+	drawBoard();
+	drawShape();
 }
 
 // create new 4X4 space
@@ -151,7 +128,7 @@ function newShape(){
   	var times = selected.join('').substring(i,i+4).match(/1/g).length;
   	if (times > len) len = times;
   }
-  currentX = Math.floor( (cols - len)/2 );
+  currentX = 6;//Math.floor( (cols - len)/2 );
 	currentY = 0;
 
   // build the selected shape array 
@@ -160,9 +137,114 @@ function newShape(){
     shape[y] = [];
     for (var x=0; x < 4; x++){
       var i = (y * 4) + x;
-      shape[y][x] = (selected[i]) ? id + 1 : 0;
+      shape[y][x] = (selected[i]) ? blockColor : 0;
     }
   }
+}
+
+// rotate the shape (clockwise)
+function rotateShape(shape){
+	var newShape = [];
+	
+	// flipping the shape
+	for (var y=0; y < 4; y++){
+		newShape[y] = [];
+		for (var x=0, i=4; x < 4; x++, i--){
+			newShape[y][x] = shape[i-1][y] || 0;
+		}
+	}
+
+	// aligning the shape to the right/middle
+	var bool = true, loop = 2;
+
+	while (bool && loop){
+		for (var x=0; x < 4; x++){
+			if (newShape[x][0] !== 0) bool = false;
+		}
+		if (bool){
+			for (var i=0; i < 4; i++){
+				newShape[i].push(0);
+				newShape[i].shift();
+			}
+		}
+		loop--;
+	}
+
+	return newShape;
+}
+
+// Adding the shape to the board Array
+function stopShape(){
+	for (var y=0; y < 4; y++){
+		for (var x=0; x < 4; x++){
+			if (shape[y][x])
+				board[currentY + y][currentX + x] = shape[y][x];
+		}
+	}
+}
+
+function clearLines(){
+	for (var row = rows - 1; row >= 0; row--){
+		var rowFilled = true;
+		for (var col=0; col < cols; col++){
+			if (!board[row][col]){
+				rowFilled = false;
+				break;
+			}
+		}
+		if (rowFilled){
+			for (var y = rows - 1; y > 0; y--){
+				board[y] = board[y-1];
+			}
+			row++;
+		}
+	}
+}
+
+// Vlidate:
+// (1) The shape is in game boarders
+// (2) The shape hit another shape
+// (3) The game didn't over yet
+// directionX / directionY are the desired moving direction
+function valid(directionX, directionY, targetShape){
+  var offsetX = currentX + directionX;
+  var offsetY = currentY + directionY;
+  var targetShape = targetShape || shape;
+
+  for (var y=0; y < 4; y++){
+    for (var x=0; x < 4; x++){
+      if (targetShape[y][x]){
+        if ( typeof board[offsetY + y] === 'undefined'
+          || typeof board[offsetY + y][offsetX + x] === 'undefined'
+          || board[offsetY + y][offsetX + x]
+          || offsetX + x < 0
+          || offsetX + x >= cols
+          || offsetY + y >= rows ){
+          
+          if (offsetY === 1) gameOver = true;
+          return false;
+        }
+      }
+    }
+  }
+
+	return true;
+}
+
+// Gameplay Interval 
+function tick(){
+	if (valid(0,1)){
+		currentY++;
+	} else {
+		if (gameOver){
+			endGame();
+			return false;
+		}
+		stopShape();
+		clearLines();
+		newShape();
+	}
+	renderDisplay();
 }
 
 // --------------------------
@@ -171,10 +253,19 @@ function newShape(){
 
 function keyPress(key){
 	switch(key){
-		case 'top'  : shape = rotate(shape); break;
-		case 'right': currentX++; break;
-		case 'down' : currentY++; break;
-		case 'left' : currentX--; break;
+		case 'top'  :
+			var rotated = rotateShape(shape);
+			if (valid(0,0,rotated)) shape = rotated;
+			break;
+		case 'right':
+			if (valid(1,0)) currentX++;
+			break;
+		case 'down' :
+			if (valid(0,1)) currentY++;
+			break;
+		case 'left' :
+			if (valid(-1,0)) currentX--;
+			break;
 	}
 }
 
@@ -206,11 +297,11 @@ function eventHandlers(){
 }
 
 // --------------------------
-//     Starts a New Game
+//        Initializing
 // --------------------------
 
 // creating a new multi-d-array with zero values
-function init() {
+function initBoard() {
   for (var y=0; y < rows; y++){
     board[y] = [];
     for (var x=0; x < cols; x++){
@@ -219,22 +310,34 @@ function init() {
   }
 }
 
+function initNextShape(){
+	$nextShape = document.getElementById('next-shape');
+	ntx = $nextShape.getContext('2d');
+}
+
+// Starts a new game
 function newGame(){
+	gameOver = false;
 	$canvas = document.getElementById('canvas');
 	ctx = $canvas.getContext('2d');
 
 	setCanvasSize();
 	eventHandlers();
 	spriteLoader();
+	initNextShape();
 
 	sprite.onload = function(){
 		clearInterval(interval);
-		init();
+		initBoard();
 		newShape();
 		renderDisplay();
 		interval = setInterval(tick, speed);
-		return setInterval(renderDisplay, speed);
 	}
+}
+
+// End the game
+function endGame(){
+	clearInterval(interval);
 }
 
 window.onload = newGame;
